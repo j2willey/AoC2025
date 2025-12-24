1985b88f9729674ce1cd4168727e85d48a4a25c1
Jim Willey <j2willey@gmail.com>
2025-12-24 09:29:37 -0800

    solution for day 3 part 2

--- 03/day03.py
+++ 03/day03.py
@@ -27,9 +27,27 @@ def day02Part1(filename = "input.txt"):
         joltage += (d1 * 10) + d2
     return joltage, "joltage"
 
+def day02Part2(filename = "input.txt"):
+    joltage = 0
+    banks = load_data(filename)
+    for bank in banks:
+        bjoltage = 0
+        ibank = [int(c) for c in bank]
+        left = 0
+        for j in range(-11,1):
+            if j == 0:
+                j = None
+            d = max(ibank[left:j])
+            i = left + bank[left:].index(str(d))
+            left = i + 1
+            bjoltage = (bjoltage * 10) + d
+        joltage += bjoltage
+    return joltage, "joltage"
+
 if __name__ == "__main__":
     filename = "input.txt"
+    # filename = "test.txt"
     part1, desc1 = day02Part1(filename)
-    # part2, desc2 = day02Part2(filename)
+    part2, desc2 = day02Part2(filename)
     print(f"part 1   {part1} : {desc1} ")
-    # print(f"part 2   {part2} : {desc2} ")
+    print(f"part 2   {part2} : {desc2} ")

c4fde1ca881db6144395529f50f89a71818717a4
Jim Willey <j2willey@gmail.com>
2025-12-29 07:41:32 -0800

    Day 11 Part 1 Solution

--- 11/day11.py
+++ 11/day11.py
@@ -1,26 +1,50 @@
 import os
 import itertools
 
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.outputs = set()
+    def add_output(self, outputs):
+        # add members of list to outputs set
+        self.outputs.update(outputs)
+
 def load_data(data_file_path):
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     data_file_path = os.path.join(script_dir, data_file_path)
-    points = []
+    nodes = {}
 
     with open(data_file_path) as f:
         for line in f:
-            points.append([ int(s) for s in line.split(',')])
+            nodename, namedoutputs = line.strip().split(":")
+            node = Node(nodename)
+            outputs = [ output.strip() for output in namedoutputs.split() ]
+            node.add_output(outputs)
+            nodes[nodename] = node
 
-    # for i, p in enumerate(points):
-    #     if i <= 20:
-    #         print(f"{i} {p}")
-
-    return points
+    return nodes
 
 
 def day11Part1(filename = "input.txt"):
+    # dfs traversal from start node "you" to end node "out"
+    # return all unique paths
+    nodes = load_data(filename)
+    queue = [ ("you", ["you"]) ]
+    unique_paths = set()
+    while queue:
+        current_node_name, path = queue.pop()
+        current_node = nodes[current_node_name]
+        for output in current_node.outputs:
+            if output == "out":
+                unique_path = path + [output]
+                unique_paths.add( tuple(unique_path) )
+            elif output not in path:
+                new_path = path + [output]
+                queue.append( (output, new_path) )
 
-    return "foo", "Part 1 result"
+    return len(unique_paths), "number of unique paths from 'you' to 'out'"
 
 
 def day11Part2(filename = "input.txt"):
